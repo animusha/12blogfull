3de24a7db66796902fbcf3d024b17ee3c390d67f
Anil Meena <anil@pensil.in>
2023-10-04 20:35:51 +0530

    Blog Project

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from './Signup'
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from './AuthLayout'
+import Input from "./Input";
+import Button from "./Button";
+import Container from "./container/Container";
+import Select from "./Select";
+
+
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    RTE,
+    Signup,
+    Login,
+    Input,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Select,
+}
